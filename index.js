0d406230733a40c77a44f15e8e95b5683b2f348a
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-05-10 03:59:39 +0400

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import addStyle from "./js/lang.js"
+import z from "./js/changelang.js"
+import event from "./js/caps.js"
+import * as module from './js/shift.js'
+import activate from "./js/activate.js"
+import animation from "./js/anim.js"
