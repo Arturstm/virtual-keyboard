d16ad0f2843af3f9e6495042d5ac73279ce592f8
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-05-10 20:36:20 +0400

    feat: link fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import addStyle from "../js/lang.js"
-import z from "../js/changelang.js"
-import event from "../js/caps.js"
-import * as module from '../js/shift.js'
-import animation from "../js/anim.js"
-import activate from "../js/activate.js"
+import addStyle from "./js/lang.js"
+import z from "./js/changelang.js"
+import event from "./js/caps.js"
+import * as module from './js/shift.js'
+import animation from "./js/anim.js"
+import activate from "./js/activate.js"
