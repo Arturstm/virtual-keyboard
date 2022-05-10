e908ad15a42f223aa7aaa10f211e87775127b52e
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-05-10 04:58:41 +0400

    feat: add js file

---
+++ js/lang.js
@@ -0,0 +1,26 @@
+const keyButtonDown = document.querySelectorAll('.caseDown');
+const keyButtonUp = document.querySelectorAll('.caseUp');
+const keyButtonCaps = document.querySelectorAll('.caps');
+const keyButtonShiftCaps = document.querySelectorAll('.shiftCaps');
+const ru = document.querySelectorAll('.rukey');
+const en = document.querySelectorAll('.enkey');
+const shift = document.querySelector('.ShiftLeft');
+
+function addStyle(e) {
+    keyButtonUp.forEach(span => {
+        span.classList.add('hidden')
+    })
+    keyButtonCaps.forEach(span => {
+        span.classList.add('hidden')
+    })
+    keyButtonShiftCaps.forEach(span => {
+        span.classList.add('hidden')
+    })
+    ru.forEach(span => {
+        span.classList.add('hidden')
+    })
+}
+
+window.addEventListener('load', addStyle);
+
+export default addStyle;
